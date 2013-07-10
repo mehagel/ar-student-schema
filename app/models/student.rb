eda2a8667cfb02b08550cd44268d448f7d874b24
Maria Pacana <maria.pacana@gmail.com>
2013-07-10 16:55:55 -0700

    Added ActiveRecord migration, added methods to Student, and added validation for Student

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,23 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  validates :email, format: {with: /.@.+\..{2,}/, message: 'only valid emails'}
+
+  validates :email, uniqueness: true
+
+  validates :age, numericality: {greater_than_or_equal_to: 5}
+
+  validates :phone, format: {with: /\d.*\d.*\d.*\d.*\d.*\d.*\d.*\d.*\d.*\d.*/, message: 'only valid phone numbers'}
+
+
+
+  def name
+     "#{self.read_attribute(:first_name)} #{self.read_attribute(:last_name)}"
+  end
+
+  def age
+    (Date.today - self.read_attribute(:birthday)).to_i / 365
+  end
+
 end
+ 
